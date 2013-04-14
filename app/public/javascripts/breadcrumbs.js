c38faa18e11dea64a40cd7997057c0dc9fcda297
Nuno Veloso <nunoveloso18@gmail.com>
2013-04-14 23:41:59 +0200

    fixed breadcrumbs

--- app/public/javascripts/breadcrumbs.js
+++ app/public/javascripts/breadcrumbs.js
@@ -1,11 +1,14 @@
 // global vars
 var $breadcrumbs = $('#breadcrumbs')
-  , pointer = get_requested_location();
+  , pointer = get_requested_location()
+  , pointer_child = pointer
+  , count = 1;
 
 // get data and populate
 while (pointer != null) {
   breadcrumb_do(pointer);
 }
+
 
 /**
  * Construct the geographical breadcrumb.
@@ -13,24 +16,8 @@ while (pointer != null) {
 function breadcrumb_do(aaid) {
 
   // creates the breadcrumb select list
-  var breadcrumb_id = 'breadcrumb-select-' + pointer;
-  $breadcrumbs.prepend('<select id="' + breadcrumb_id + '" class="chosen-select" data-placeholder="Choose a Place..."></select>');
-
-  // get own object and update pointer
-  $.ajax({
-    type: "GET",
-    url: '/geo/' + aaid + '/json',
-    async: false,
-    dataType: "json",
-    context: document.body
-  }).done(function(admin_area) {
-    if (admin_area && 'parent_id' in admin_area) {
-      pointer = admin_area.parent_id
-    }
-    else {
-     pointer = null;
-    }
-  });
+  var breadcrumb_id = 'breadcrumb-select-' + aaid;
+  $breadcrumbs.prepend('<select id="' + breadcrumb_id + '" class="chzn-select" data-placeholder="Choose a Place..."><option value=""></option></select>');
 
   // load the admin areas for that pointer and process them
   $.ajax({
@@ -49,7 +36,14 @@ function breadcrumb_do(aaid) {
 
     // read through the json data
     $.each(admin_areas, function() {
+      // build it!
       $breadcrumbs.find('#' + breadcrumb_id).append('<option value="' + this.aaid + '">' + this.name + '</option> ');
+
+      // whether or not we want to select the option
+      if (pointer_child == this.aaid) {
+        $breadcrumbs.find('#' + breadcrumb_id).find('option[value="' + this.aaid + '"]').attr('selected', 'selected');
+      }
+
     });
 
     // "chosenify" it
@@ -57,7 +51,7 @@ function breadcrumb_do(aaid) {
       .find('#' + breadcrumb_id)
       .chosen({
         no_results_text: "No results matched for",
-        allow_single_deselect: true
+        allow_single_deselect: false
       })
       .change(function(){
         // similar behavior as clicking on a link
@@ -66,6 +60,27 @@ function breadcrumb_do(aaid) {
       });
 
   });
+
+
+  pointer_child = pointer;
+
+  // get own object and update pointer
+  $.ajax({
+    type: "GET",
+    url: '/geo/' + aaid + '/json',
+    async: false,
+    dataType: "json",
+    context: document.body
+  }).done(function(admin_area) {
+    if (admin_area && 'parent_id' in admin_area) {
+      pointer = admin_area.parent_id
+    }
+    else {
+     pointer = null;
+    }
+
+  });
+
 }
 
 function get_requested_location() {
