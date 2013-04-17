85bd2ed781eeadae6172c175905b0dea5be4136a
Nuno Veloso <nunoveloso18@gmail.com>
2013-04-18 00:49:17 +0200

    moved views to app subfolder

--- app/app.js
+++ app/app.js
@@ -29,7 +29,7 @@ app.engine('ejs', engine);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
+  app.set('views', __dirname + '/app/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
