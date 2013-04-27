823f8a3ba946375b519d86752fe10ec34105ba62
Nuno Veloso <nunoveloso18@gmail.com>
2013-04-28 01:31:05 +0200

    sandbox for Sside breadcrumbs

--- app/app/controllers/geoadminareas.js
+++ app/app/controllers/geoadminareas.js
@@ -22,7 +22,7 @@ exports.geoadminarea = function(req, res, next, aaid){
     if (err) return next(err)
     if (!geoadminarea && aaid != 0) return next(new Error('Failed to load administrative area with the code: ' + aaid))
     req.geoadminarea = geoadminarea
-  console.log(geoadminarea);
+  // console.log(geoadminarea);
     next()
   })
 }
@@ -48,18 +48,78 @@ exports.json = function(req, res){
  * API: sends JSON of all the chidren of a given GeoAdminArea
  */
 exports.json_children = function(req, res){
+  // query criteria
   var options = {
     criteria: { parent_id: req.params.aaid },
-    fields: { aaid: 1, name: 1}
+    fields: { aaid: 1, name: 1 }
   }
 
+  // get the list of requested elements
   GeoAdminArea.list(options, function(err, geoadminareas) {
+    // error handling
     if (err) return res.render('500')
+
+    // execute!
     GeoAdminArea.count().exec(function (err, count) {
+      // send JSON
       res.send(geoadminareas)
     })
   })
 };
 
+
+/**
+ * API: sends JSON of
+ */
+exports.breadcrumb = function(req, res){
+  // variables
+  var breadcrumbs = []
+
+  // recursively generates the breadcumb trail
+  breadcumb_trail(req.params.aaid);
+
+
+  /**
+   * Recursively generate the breadcrumb trail, render when done.
+   * @param aaid [int] if the current pointer
+   */
+  function breadcumb_trail(aaid) {
+    // load current AA trail pointer
+    GeoAdminArea.load(aaid, function (err, aa_trail) {
+      // error handling
+      if (err) return res.render('500')
+
+      // query criteria
+      var options = {
+        criteria: { parent_id: aa_trail.parent_id },
+        fields: { aaid: 1, name: 1 }
+      }
+
+      // get the list of requested elements
+      GeoAdminArea.list(options, function(err, sibling_aa) {
+        // error handling
+        if (err) return res.render('500')
+
+        // execute!
+        GeoAdminArea.count().exec(function (err, count) {
+
+          breadcrumbs.unshift({
+            select: aa_trail,
+            list : sibling_aa,
+          });
+
+          if (aa_trail.parent_id == 0) {
+            res.render('index', { title: req.geoadminarea.name, breadcrumbs: breadcrumbs });
+            // res.send(breadcrumbs)
+          }
+          else {
+            breadcumb_trail(aa_trail.parent_id)
+          }
+
+        })
+      })
+    })
+  }
+};
 
 
