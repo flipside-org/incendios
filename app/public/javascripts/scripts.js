3c224f99a858f2beafeb237b90481e32460c0b43
Daniel Silva <daniel.silva@flipside.org>
2013-05-23 15:37:03 +0100

    Updates layout with new structure.

--- app/public/javascripts/scripts.js
+++ app/public/javascripts/scripts.js
@@ -1,4 +1,9 @@
+$(window).resize(function(){ set_map_height(); });
+
 $(document).ready(function() {
+  // Init!
+  set_sidebar_height();
+  set_map_height();
 
   var aaid = get_requested_aaid();
   var stats_admin_area = get_json('/stats/' + aaid + '/json');
@@ -154,12 +159,57 @@ $(document).ready(function() {
     lineColors: ['#2aa198'],
     pointFillColors: ['#268bd2']
   });
+  
+  // This must be the last thing to do because the
+  // content will change the sidebar height.
+  set_map_height();
+  
 });
-
 
 /***********************************************************/
 // HELPERS
 /***********************************************************/
+
+/*
+ * Resizes the sidebar based on the size of the window.
+ */
+function set_sidebar_height() {
+  // Resize only if window > 850
+  var $window = $(window);
+  if ($window.width() <= 850){
+    return false;
+  }
+  
+  var w_height = $window.height();
+  var $sidebar = $('#sidebar');
+  var oh_header = $('#header').outerHeight();
+  var oh_sidebar = $sidebar.outerHeight();
+  var oh_footer = $('#footer').outerHeight();
+  
+  // If all the sidebar elements together don't match the window height
+  // adjust the sidebar.
+  if (oh_header + oh_sidebar + oh_footer < w_height) { console.log(w_height);
+    // We are setting the height for the sidebar. We need to account for
+    // the outer height like padding, border...
+    var final_height = w_height - oh_footer - oh_header - (oh_sidebar - $sidebar.height());
+    $sidebar.css('min-height', final_height);
+  }
+}
+
+/*
+ * Resizes the map based on the height of the sidebar.
+ */
+function set_map_height() {
+  // Resize only if window > 850
+  if ($(window).width() <= 850){
+    return false;
+  }
+  var oh_header = $('#header').outerHeight();
+  var oh_sidebar = $('#sidebar').outerHeight();
+  var oh_footer = $('#footer').outerHeight();
+  
+  $('#map').height(oh_header + oh_sidebar + oh_footer);
+}
 
 /**
  * @param string url
